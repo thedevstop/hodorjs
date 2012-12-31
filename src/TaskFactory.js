afe73b2c2c0a8abfc26b31ff90839c13932cea2b
David Ruttka <david@davidruttka.com>
2012-12-31 16:33:35 -0600

    Added comments to TaskFactory.js

--- src/TaskFactory.js
+++ src/TaskFactory.js
@@ -1,10 +1,12 @@
 var Hodor = Hodor || {};
 
-// In the browser, reference Task.js. In node / mocha testing, we'll require it
+// In the browser, there should be a preceding reference to Task.js. 
+// Running the tests in mocha on node, we'll use require to bring it in.
 var Task = Hodor.Task || require("./Task.js");
 
 Hodor.TaskFactory = (function(){
-
+  
+  // object literal should do for now??
   var TaskFactory = {
     create: function(f){
       return new Task(f);
